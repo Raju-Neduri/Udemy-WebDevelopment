4db896aff836a245a195d8788637134ec4d9bcb6
Raju-Neduri <rajneduri123@gmail.com>
2025-04-28 12:16:18 +0530

    updated

--- JavaScript_Youtube/DOM Challenge Starting Files/index.js
+++ JavaScript_Youtube/DOM Challenge Starting Files/index.js
@@ -1,1 +1,7 @@
-document.query
+document.getElementById("title").innerHTML = "DOM Manipulation Challenge";
+document.querySelector("li a").innerHTML="Raju";
+document.querySelector("li a").style.color="red";
+document.querySelector("li a").style.fontSize="30px";
+document.getElementById("title").style.color="blue";
+
+document.querySelector("button").style.backgroundColor="Yellow";
